81f630c83de9cf95686e396e67ee4f905742e12f
Dimitrios Kokkonias <dimi.kokkonias@gmail.com>
2024-02-03 19:53:28 +0100

    🐛 Fix port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: ['*', 'http://localhost:4200'], methods: ['GET', 'PATCH'] });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
-  await app.listen(process.env.port || 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
