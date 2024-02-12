e4fc2b16c30ecdf438e0908b2915f333724d9012
Dimitrios Kokkonias <dimi.kokkonias@gmail.com>
2024-02-12 16:05:19 +0100

    ⚡️ Add rate limit to code endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,21 @@ import { GuestsModule } from './guests/guests.module';
 import { PrivateDataModule } from './private-data/private-data.module';
 import { CodeModule } from './code/code.module';
 import { SubscriptionModule } from './subscription/subscription.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION_STRING), GuestsModule, PrivateDataModule, CodeModule, SubscriptionModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 30000,
+        limit: 3,
+      },
+    ]),
+    MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION_STRING),
+    GuestsModule,
+    PrivateDataModule,
+    CodeModule,
+    SubscriptionModule,
+  ],
 })
 export class AppModule {}
--- src/code/code.module.ts
+++ src/code/code.module.ts
@@ -4,10 +4,18 @@ import { GuestsService } from 'src/guests/guests.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Guests, GuestsSchema } from 'src/guests/guests.schema';
 import { CodeController } from './code.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Guests.name, schema: GuestsSchema }])],
   controllers: [CodeController],
-  providers: [GuestsService],
+  providers: [
+    GuestsService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class CodeModule {}
