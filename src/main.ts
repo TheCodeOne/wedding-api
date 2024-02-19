65bfac6fca32db28427cad51d2fb746f4003791f
Dimitrios Kokkonias <dimi.kokkonias@gmail.com>
2024-02-19 11:50:12 +0100

    ✨ Add kokksl.at to cors whitelist

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['*', 'http://localhost:4200', 'https://wedding-frontend-3a53ef0974fe.herokuapp.com'], methods: ['GET', 'PATCH'] });
+  app.enableCors({ origin: ['*', 'http://localhost:4200', 'https://wedding-frontend-3a53ef0974fe.herokuapp.com', 'https://kokksl.at'], methods: ['GET', 'PATCH'] });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(process.env.PORT || 3000);
 }
